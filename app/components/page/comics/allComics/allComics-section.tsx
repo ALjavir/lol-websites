27866f9529085f37d1107d2e74fdc0ec61b5f205
ALjavir <flutter.user.tonmoy@gmail.com>
2026-08-15 22:38:17 +0600

    all page completed

--- app/components/page/comics/allComics/allComics-section.tsx
+++ app/components/page/comics/allComics/allComics-section.tsx
@@ -1,53 +1,70 @@
 import { useState, type useEffect } from "react";
+import "./allComics-style.css"
+import { ButtonBlue } from "~/components/common/button/button";
+
 
 interface allComDataInt {
-    allComdata: object[]
+    allComdata: any
 }
-
-
 export default function AllComics({ allComdata }: allComDataInt) {
-    
     const [sortBy, setSortBy] = useState("recent");
-
-   const [displayedCom, setdisplayedCom] = useState<any[]>([]);
-
+    const [displayedCom, setdisplayedCom] = useState<any[]>(allComdata[sortBy].data);
     var comDataMap = [
+
         {
             label: "Recently Added",
             cat: "recent"
         },
         {
             label: "One Shot",
-            cat:"one-shots"
+            cat: "one-shots"
         },
         {
             label: "Comic Series",
-           cat: "series"
+            cat: "series"
         },
     ];
-     
-     
 
-console.log(allComdata.every)
-
+    console.log(allComdata)
 
     return (
         <section id="all-com-sec">
-              {/* <div className="all-news-filter-btn gold-sort-btn">
+            <div className="all-news-filter-btn gold-sort-btn">
                 {comDataMap.map((btnData) => (
                     <button
                         key={btnData.cat}
                         className={sortBy === btnData.cat ? "active" : ""}
-                        onClick={() => { setSortBy(btnData.cat) }}
-                    >
-                        {btnData.label} | {displayedCom.length}
+                        onClick={() => {
+                            setSortBy(btnData.cat);
+                            setdisplayedCom(allComdata[btnData.cat].data);
 
+                        }}
+                    >
+                        {btnData.label} | {allComdata[btnData.cat].data.length}
                     </button>
                 ))}
 
-            </div> */}
+            </div>
+
+            <div className="all-com-grid">
+                {displayedCom.map((com, index) => (
+                    <div className="all-com-card" key={index} >
+                        <img src={com.background.uri} alt={com.background.title} />
+                        <div className="all-com-card-info">
+                            <h1>{com.title}</h1>
+                            <div className="all-com-sub-card">
+                                <p>{com.subtitle ?? com.id}</p>
+                              
+                              <p className="">❯</p>
+                            </div>
+                    
+
+                        </div>
+
+                    </div>
+                ))}
+            </div>
+
         </section>
     )
-
-
 }
